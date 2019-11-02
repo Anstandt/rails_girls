5bbde42dd20db21053c91b79f4e4ade7471f0b19
Veronika <veronika@MacBook-Pro-1.local>
2019-11-02 15:53:36 +0300

    no errors in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,12 +37,12 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  config.active_support.deprecation = :log
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.active_record.verbose_query_logs = true
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
